813fc2c806cc33cd087b3937ca502c6c8855edae
TrailBlazers-frontend1 <tblazersfrontend1@gmail.com>
2022-10-29 10:31:08 +0630

    adadd

--- resources/views/trainer/layouts/header.blade.php
+++ resources/views/trainer/layouts/header.blade.php
@@ -15,14 +15,16 @@
                 </select>
             </div>
 
+            <div class="theme-contaier">
+                <select class="theme">
+                    <option value="light">Light</option>
+                    <option value="dark">Dark</option>
+                    <option value="pink">Pink</option>
+                </select>
+            </div>
+
         </div>
-        <div class="theme-contaier">
-            <select class="theme">
-                <option value="light">Light</option>
-                <option value="dark">Dark</option>
-                <option value="pink">Pink</option>
-            </select>
-        </div>
+
 
         <div class="customer-navlinks-container">
             <a href="#">Home</a>
