5f6e128a6b7509ca4f04e8042713e7cb4ac33972
thandarannie <thandarannie12@gmail.com>
2022-11-01 11:08:22 +0630

    dd

--- resources/views/customer/layouts/header.blade.php
+++ resources/views/customer/layouts/header.blade.php
@@ -13,18 +13,21 @@
                 </div>
 
                 <select>
-                    <option value="">Myanmar</option>
-                    <option value="">English</option>
+                    <option value=""><a href={{route('locale','en')}}>English</a></option>
+                    <option value=""><a href={{route('locale','mm')}}>Myanmar</a></option>
+
                 </select>
+                {{-- <a href={{route('locale','en')}}>English</a>
+                <a href={{route('locale','mm')}}>Myanmar</a> --}}
             </div>
 
         </div>
         <div class="customer-navlinks-container">
             @guest
-            <a href="{{route('home')}}">Home</a>
+            <a href="{{route('home')}}">{{__('msg.home')}}</a>
             @endguest
             @auth
-            <a href="{{route('social_media')}}">Home</a>
+            <a href="{{route('social_media')}}">{{__('msg.home')}}</a>
             @endauth
             <a href="#">Shop</a>
             {{-- <a href="#">Training Center</a> --}}
