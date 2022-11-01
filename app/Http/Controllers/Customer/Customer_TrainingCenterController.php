db42ea2ab2172c8c784a824e9689075da71c52d7
htet-yadanar <htetyatanaraung@gmail.com>
2022-11-01 13:33:14 +0630

    dd

--- app/Http/Controllers/Customer/Customer_TrainingCenterController.php
+++ app/Http/Controllers/Customer/Customer_TrainingCenterController.php
@@ -309,9 +309,11 @@ class Customer_TrainingCenterController extends Controller
     public function water_track()
     {
         $current_date = Carbon::now()->toDateString();
-        $water = WaterTracked::where('date', $current_date)->first();
 
         $user = auth()->user();
+
+        $water = WaterTracked::where('user_id', $user->id)->where('date', $current_date)->first();
+
 
         if(!$water) {
             $water = new WaterTracked();
@@ -326,10 +328,13 @@ class Customer_TrainingCenterController extends Controller
             ]);
         }else{
             $water = WaterTracked::findOrFail($water->id);
-            $water->user_id = $user->id;
+            if($water->update_water == 3000) {
+                return response()->json([
+                    "message" => "You cant drink anymore!"
+                ]);
+            }
             $water->update_water += 250;
-            $water->date = $current_date;
-            $water->save();
+            $water->update();
 
             return response()->json([
                 'success' => 200,
