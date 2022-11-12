9fe77421cc4bd3c87d88cf8b18abbbd7d3dc1512
thandarannie <thandarannie12@gmail.com>
2022-11-12 11:48:05 +0630

    d

--- database/seeders/MemberTypeSeeder.php
+++ database/seeders/MemberTypeSeeder.php
@@ -15,52 +15,111 @@ class MemberTypeSeeder extends Seeder
     public function run()
     {
         Member::create([
-            'member_type' =>'Free',
-            'price'=>0,
-            'duration'=>0,
-            'role_id'=>4
-           ]);
-
-        Member::create([
         'member_type' =>'Platinum',
         'duration'=>1,
         'price'=>5000,
-        'role_id'=>5
+        'role_id'=>4,
+        'pros'=>'adipisicing elit, Dolore fugit hic,ullam cumque',
+        'cons'=>'sequi est, quod'
        ]);
 
        Member::create([
         'member_type' =>'Gold',
         'duration'=>1,
         'price'=>20000,
-        'role_id'=>6
+        'role_id'=>5,
+        'pros'=>'adipisicing elit, Dolore fugit hic,ullam cumque',
+        'cons'=>'sequi est, quod'
        ]);
 
        Member::create([
         'member_type' =>'Diamond',
         'duration'=>1,
         'price'=>40000,
-        'role_id'=>7
+        'role_id'=>6,
+        'pros'=>'adipisicing elit, Dolore fugit hic,ullam cumque',
+        'cons'=>'sequi est, quod'
        ]);
 
        Member::create([
         'member_type' =>'Ruby',
         'duration'=>1,
         'price'=>100000,
-        'role_id'=>8
+        'role_id'=>7,
+        'pros'=>'adipisicing elit, Dolore fugit hic,ullam cumque',
+        'cons'=>'sequi est, quod'
        ]);
 
        Member::create([
         'member_type' =>'Ruby Premium',
         'duration'=>1,
         'price'=>200000,
-        'role_id'=>9
+        'role_id'=>8,
+        'pros'=>'adipisicing elit, Dolore fugit hic,ullam cumque',
+        'cons'=>'sequi est, quod'
        ]);
 
        Member::create([
         'member_type' =>'Gym Member',
         'price'=>40000,
         'duration'=>1,
-        'role_id'=>11
+        'role_id'=>10,
+        'pros'=>'adipisicing elit, Dolore fugit hic,ullam cumque',
+        'cons'=>'sequi est, quod'
+       ]);
+
+        Member::create([
+        'member_type' =>'Platinum',
+        'duration'=>3,
+        'price'=>12000,
+        'role_id'=>4,
+        'pros'=>'adipisicing elit, Dolore fugit hic,ullam cumque',
+        'cons'=>'sequi est, quod'
+       ]);
+
+       Member::create([
+        'member_type' =>'Gold',
+        'duration'=>3,
+        'price'=>50000,
+        'role_id'=>5,
+        'pros'=>'adipisicing elit, Dolore fugit hic,ullam cumque',
+        'cons'=>'sequi est, quod'
+       ]);
+
+       Member::create([
+        'member_type' =>'Diamond',
+        'duration'=>3,
+        'price'=>100000,
+        'role_id'=>6,
+        'pros'=>'adipisicing elit, Dolore fugit hic,ullam cumque',
+        'cons'=>'sequi est, quod'
+       ]);
+
+       Member::create([
+        'member_type' =>'Ruby',
+        'duration'=>3,
+        'price'=>250000,
+        'role_id'=>7,
+        'pros'=>'adipisicing elit, Dolore fugit hic,ullam cumque',
+        'cons'=>'sequi est, quod'
+       ]);
+
+       Member::create([
+        'member_type' =>'Ruby Premium',
+        'duration'=>3,
+        'price'=>500000,
+        'role_id'=>8,
+        'pros'=>'adipisicing elit, Dolore fugit hic,ullam cumque',
+        'cons'=>'sequi est, quod'
+       ]);
+
+       Member::create([
+        'member_type' =>'Gym Member',
+        'price'=>100000,
+        'duration'=>3,
+        'role_id'=>10,
+        'pros'=>'adipisicing elit, Dolore fugit hic,ullam cumque',
+        'cons'=>'sequi est, quod'
        ]);
 
     }
