a56a029e145d592f745be51b0133e2746b69ab23
Ye99-AungKhant <yeaungkhant077@gmail.com>
2022-12-09 14:31:09 +0630

    sjkdfijsdufiousdfoui

--- resources/views/customer/group_chat-detail.blade.php
+++ resources/views/customer/group_chat-detail.blade.php
@@ -1,104 +1,121 @@
 @extends('customer.layouts.app_home')
 @section('content')
-<div class="social-media-right-container">
 
-    <div class="modal fade" id="createGroupModal" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">Add Members</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-              <form action="{{route('socialmedia.group.addmember',$id)}}" class="create-group-form" method="POST">
-                @csrf
-                <div class="create-group-addfris">
-                    <p>Add Your Friends</p>
-                    <select class="js-example-basic-multiple" name="members[]" multiple="multiple">
-                        @foreach ($friends as $friend)
-                            <option value="{{$friend->id}}">{{$friend->name}}</option>
-                        @endforeach
-                      </select>
+    <div class="social-media-right-container">
+
+        <div class="modal fade" id="createGroupModal" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Add Members</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form action="{{ route('socialmedia.group.addmember', $id) }}" class="create-group-form"
+                            method="POST">
+                            @csrf
+                            <div class="create-group-addfris">
+                                <p>Add Your Friends</p>
+                                <select class="js-example-basic-multiple" name="members[]" multiple="multiple">
+
+                                    @if ($members == null || count($members) == 0)
+                                        @foreach ($friends as $friend)
+                                            <option value="{{ $friend->id }}">{{ $friend->name }}</option>
+                                        @endforeach
+                                    @else
+                                        @foreach ($friends as $friend)
+                                            @foreach ($members as $item)
+                                                @if ($item->member_id == $friend->id)
+                                                @elseif(count($members) == count($friends))
+                                                @else
+                                                    <option value="{{ $friend->id }}">{{ $friend->name }}</option>
+                                                @endif
+                                            @endforeach
+                                        @endforeach
+                                    @endif
+
+
+
+                                </select>
+                            </div>
+
+                            <button type="submit" class="customer-primary-btn create-group-submit-btn">Create</button>
+                        </form>
+                    </div>
+
                 </div>
+            </div>
+        </div>
 
-                <button type="submit" class="customer-primary-btn create-group-submit-btn">Create</button>
-              </form>
+
+        <div class="group-chat-header">
+            <div class="group-chat-header-name-container">
+                <a href="{{ route('socialmedia.group', $group->id) }}" class="group-chat-header-name-container">
+                    <img src="../imgs/avatar.png" />
+                    <div class="group-chat-header-name-text-container">
+                        <p>{{ $group->group_name }}</p>
+                    </div>
+                </a>
+            </div>
+            <div class="chat-header-call-icons-container">
+                <iconify-icon icon="ant-design:phone-outlined" class="chat-header-phone-icon"></iconify-icon>
+                <iconify-icon icon="eva:video-outline" class="chat-header-video-icon"></iconify-icon>
+
+                <a href="../htmls/trainerTrainingCenterViewMedia.html" class="group-chat-view-midea-link">
+                    <p>View Media</p>
+                    <iconify-icon icon="akar-icons:arrow-right" class="group-chat-view-midea-link-icon"></iconify-icon>
+                </a>
             </div>
 
-          </div>
-        </div>
-    </div>
 
-
-    <div class="group-chat-header">
-        <div class="group-chat-header-name-container">
-            <a href="{{route('socialmedia.group',$group->id)}}" class="group-chat-header-name-container">
-                <img src="../imgs/avatar.png"/>
-                <div class="group-chat-header-name-text-container">
-                    <p>{{$group->group_name}}</p>
-                </div>
-            </a>
-        </div>
-        <div class="chat-header-call-icons-container">
-            <iconify-icon icon="ant-design:phone-outlined" class="chat-header-phone-icon"></iconify-icon>
-            <iconify-icon icon="eva:video-outline" class="chat-header-video-icon"></iconify-icon>
-
-            <a href="../htmls/trainerTrainingCenterViewMedia.html" class="group-chat-view-midea-link">
-                <p>View Media</p>
-                <iconify-icon icon="akar-icons:arrow-right" class="group-chat-view-midea-link-icon"></iconify-icon>
-            </a>
         </div>
 
+        <button type="button" class="social-media-allchats-header-add-btn customer-primary-btn group-chat-add-btn"
+            data-bs-toggle="modal" data-bs-target="#createGroupModal">
+            <iconify-icon icon="akar-icons:circle-plus" class="social-media-allchats-header-plus-icon"></iconify-icon>
+            <p>Add Member</p>
+        </button>
 
-    </div>
-
-    <button type="button" class="social-media-allchats-header-add-btn customer-primary-btn group-chat-add-btn" data-bs-toggle="modal" data-bs-target="#createGroupModal">
-        <iconify-icon icon="akar-icons:circle-plus" class="social-media-allchats-header-plus-icon"></iconify-icon>
-        <p>Group</p>
-    </button>
-
-    <div class="social-media-view-members-container">
-        @forelse ($members as $member)
-
-            <form action="{{route('socialmedia.group.memberkick')}}" method="post">
-                @csrf
-                <input type="text" value="{{$member->group_id}}" name="groupId" hidden>
-                <input type="text" value="{{$member->member_id}}" name="memberId" hidden>
+        <div class="social-media-view-members-container">
+            @forelse ($members as $member)
+                <form action="{{ route('socialmedia.group.memberkick') }}" method="post">
+                    @csrf
+                    <input type="text" value="{{ $member->group_id }}" name="groupId" hidden>
+                    <input type="text" value="{{ $member->member_id }}" name="memberId" hidden>
+                    <div class="social-media-view-memers-row">
+                        <div class="social-media-view-memers-row-name">
+                            @if ($member->user->user_profile != null && $member->user->user_profile->profile_image != null)
+                                <img src="{{ asset('storage/post' . $member->user->user_profile->profile_image) }}">
+                            @else
+                                <img class="nav-profile-img" src="{{ asset('img/customer/imgs/user_default.jpg') }}" />
+                            @endif
+                            <p>{{ $member->user->name }}</p>
+                        </div>
+                        <div class="social-media-view-members-row-btns">
+                            <a href="{{ route('socialmedia.profile', $member->member_id) }}"
+                                class="customer-secondary-btn">View Profile</a>
+                            <button type="submit" class="customer-red-btn">Kick</button>
+                        </div>
+                    </div>
+                </form>
+            @empty
                 <div class="social-media-view-memers-row">
-                    <div class="social-media-view-memers-row-name">
-                        @if ($member->user->profiles[0]->profile_image != null)
-                            <img src="{{asset('storage/post'.$member->user->profiles[0]->profile_image)}}">
-                        @else
-                            <img class="nav-profile-img" src="{{asset('img/customer/imgs/user_default.jpg')}}"/>
-                        @endif
-                        <p>{{$member->user->name}}</p>
-                    </div>
-                    <div class="social-media-view-members-row-btns">
-                        <a href="{{route('socialmedia.profile',$member->member_id)}}" class="customer-secondary-btn">View Profile</a>
-                        <button type="submit" class="customer-red-btn">Kick</button>
-                    </div>
+                    <p>Empty group member. Please add member.</p>
                 </div>
-            </form>
-        @empty
-            <div class="social-media-view-memers-row">
-                <p>Empty group member. Please add member.</p>
-            </div>
-        @endforelse
+            @endforelse
 
 
+        </div>
     </div>
-</div>
 @endsection
 @push('scripts')
-<script>
+    <script>
+        $(document).ready(function() {
+            $('.js-example-basic-multiple').select2({
+                dropdownParent: "#createGroupModal"
+            });
 
-    $(document).ready(function(){
-        $('.js-example-basic-multiple').select2(
-                { dropdownParent: "#createGroupModal" }
-        );
-
-        $('.select2-container').attr('style', '');
-    })
-
-</script>
+            $('.select2-container').attr('style', '');
+        })
+    </script>
 @endpush
