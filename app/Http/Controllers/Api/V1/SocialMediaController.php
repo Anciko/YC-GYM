9d653510d19ebc07d8b1d4b7e08f206175cd907f
htet-yadanar <htetyatanaraung@gmail.com>
2022-12-13 13:55:50 +0630

    dd

--- app/Http/Controllers/Api/V1/SocialMediaController.php
+++ app/Http/Controllers/Api/V1/SocialMediaController.php
@@ -1360,7 +1360,7 @@ class SocialMediaController extends Controller
             env('PUSHER_APP_ID'),
             $options
             );
-        $pusher->trigger('chatting.'.auth()->user()->id.$to_user_id, 'chatting-event', $message);
+        $pusher->trigger('chatting.'.auth()->user()->id.'.'.$to_user_id, 'chatting-event', $message);
         //broadcast(new Chatting($message, $request->sender));
 
         $user_id=auth()->user()->id;
