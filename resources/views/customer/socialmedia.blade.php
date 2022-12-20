954a90c528732e5fded810b13253232b67b59f22
thandarannie <thandarannie12@gmail.com>
2022-12-20 10:36:36 +0630

    conflict

--- resources/views/customer/socialmedia.blade.php
+++ resources/views/customer/socialmedia.blade.php
@@ -247,32 +247,8 @@
 </div>
 
 @endsection
-<<<<<<< HEAD
 @push('scripts')
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jscroll/2.4.1/jquery.jscroll.min.js"></script>
-
 <script>
-=======
-
-@push('scripts')
-
-<script type="text/javascript">
-    $('ul.pagination').hide();
-    $(function() {
-        $('.infinite-scroll').jscroll({
-            autoTrigger: true,
-            // loadingHtml: '<img class="center-block" src="/images/loading.gif" alt="Loading..." />', // MAKE SURE THAT YOU PUT THE CORRECT IMG PATH
-            padding: 0,
-            nextSelector: '.pagination li.active + li a',
-            contentSelector: 'div.infinite-scroll',
-            callback: function() {
-                $('ul.pagination').remove();
-            }
-        });
-    });
-</script>
-    <script>
->>>>>>> ad214c2e540d36aebcac1b665dcb2231b622f397
 
     $(document).ready(function() {
 
