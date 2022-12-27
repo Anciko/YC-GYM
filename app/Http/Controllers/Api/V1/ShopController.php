63897c35d852f296aa90321dcd271f4c752d279a
htet-yadanar <htetyatanaraung@gmail.com>
2022-12-27 13:12:17 +0630

    dd

--- app/Http/Controllers/Api/V1/ShopController.php
+++ app/Http/Controllers/Api/V1/ShopController.php
@@ -297,15 +297,17 @@ class ShopController extends Controller
         $user_id = auth()->user()->id;
         $shop_id = $request->shop_id;
         $shop_rating = ShopRating::where('user_id', $user_id)->where('shop_id',$shop_id)->first();
-        if($shop_rating){
-            DB::table('shop_ratings')->where('user_id', $user_id)->where('shop_id',$shop_id)->update(['rating' => $request->rating]);
-        }
-        else{
-            $shop_rating = new ShopRating();
-            $shop_rating->user_id = $user_id;
-            $shop_rating->shop_id = $shop_id;
-            $shop_rating->rating = $request->rating;
-            $shop_rating->save();
+        if($shop_rating->shop_id != auth()->user()->id){
+            if($shop_rating){
+                DB::table('shop_ratings')->where('user_id', $user_id)->where('shop_id',$shop_id)->update(['rating' => $request->rating]);
+            }
+            else{
+                $shop_rating = new ShopRating();
+                $shop_rating->user_id = $user_id;
+                $shop_rating->shop_id = $shop_id;
+                $shop_rating->rating = $request->rating;
+                $shop_rating->save();
+            }
         }
         return response()->json([
             'success' => 'rated'
